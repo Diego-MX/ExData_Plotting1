eff448f7c659163e72c3fc2162f1196fc241e117
Diego <diego.villamil@konfio.mx>
2015-03-08 13:07:39 -0600

    Update plot4.R

--- src/plot4.R
+++ src/plot4.R
@@ -15,15 +15,15 @@ setwd("../figure")
 png("plot4.png", width=480, height=480, units="px")
 with(household, {par(mfrow=c(2,2))
 
-    # First Corner
+    # First Plot
     plot(Time, Global_active_power, type="l", col="black",
         xlab="", ylab="Global Active Power")
 
-    # Second Corner
+    # Second Plot
     plot(Time, Voltage, type="l", col="black",
         xlab="datetime")
 
-    # Third Corner
+    # Third Plot
     plot(Time, Sub_metering_1, type="l", col="black", xlab="",
          ylab="Energy sub metering")
     lines(Time, Sub_metering_2, col="red")
@@ -31,7 +31,8 @@ with(household, {par(mfrow=c(2,2))
     legend("topright", lty=1, col=c("black", "red", "blue"),
         legend=c("Sub_metering_1", "Sub_metering_2",
             "Sub_metering_3"))
-
+    
+    # Fourth Plot
     plot(Time, Global_reactive_power, type="l",
         xlab="datetime")})
 
